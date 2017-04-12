b24120f09e713e87c3f89535b61a1676d5e08bc9
sarahkasiske <s.kasiske@umiami.edu>
2017-04-12 11:01:09 -0400

    Adding First JavaScript Object

---
+++ js/breeds.js
@@ -0,0 +1,12 @@
+// Dog Breeds
+var goldenRetriever = {
+  breed: "Golden Retriever",
+  description: "Good Dog",
+  characteristics: "Happy, Loyal",
+  type: "Sporting",
+  good_with_kids_rating: 4,
+  rareBreed: false,
+  hypoallergenic: false,
+}
+
+var breeds = []; breeds.push(goldenRetriever)
